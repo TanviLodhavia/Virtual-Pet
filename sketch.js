dcacf0e22cf0bf57685870a09d0f404283a95a78
TanviLodhavia <65407346+TanviLodhavia@users.noreply.github.com>
2020-07-13 20:15:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 //Create variables here
 var dog, happyDog, database, foodS, foodStock;
+foodS = 0;
 function preload()
 {
   //load images here
@@ -10,8 +11,9 @@ function preload()
 
 function setup() {
   createCanvas(500, 500);
-  dog = createSprite(250,250,20,20);
-  dog.addImage("dog",dog);
+  dog1 = createSprite(250,250,20,20);
+  dog1.addImage(dog);
+  dog1.scale = 0.30;
   database = firebase.database();
   foodStock = database.ref('Food');
   foodStock.on("value", readStock);
@@ -22,12 +24,17 @@ function setup() {
 function draw() {  
   background(46,139,87);
   drawSprites();
+
   //add styles here
   if (keyWentDown(UP_ARROW)){
     writeStock(foodS);
-    dog.addImage("happy",happyDog);
+    dog1.addImage(happyDog);
   }
-    text(foodS,100,200);
+    fill("red");
+    text(foodS,250,50);
+
+    fill ("blue");
+    text("Note: Press 'UP ARROW' key to feed Padfoot", 135, 400);
   
 }
 
@@ -40,5 +47,3 @@ function writeStock(x){
     Food:x
   })
 }
-
-
